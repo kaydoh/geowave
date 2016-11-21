e82091da3f99b5b4c3f1b1c97f918accc8958501
Kent Miller <kmiller@radiantblue.com>
2016-11-21 15:26:17 -0800

    remove -e option from script

--- .utility/gcloud-init.sh
+++ .utility/gcloud-init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ev
+set -v
 
 if [ "$STORE_TYPE" == "BIGTABLE" ]; then
 	if [ ! -d ./google-cloud-sdk ]; then
@@ -9,7 +9,7 @@ if [ "$STORE_TYPE" == "BIGTABLE" ]; then
 		tar xvf google-cloud-sdk-135.0.0-linux-x86_64.tar
 		rm google-cloud-sdk-135.0.0-linux-x86_64.tar
 	fi
-	
+
 	# install the beta component & emulator
 	cat <<EOF | ./google-cloud-sdk/bin/gcloud components install beta
 Y
