ccc5c7e3ef8c1b8af8713d85e6962227d4adcb97
Rich Fecher <rfecher@gmail.com>
2016-12-08 08:36:19 -0500

    wip

--- deploy/packaging/docker/publish-common-rpm.sh
+++ deploy/packaging/docker/publish-common-rpm.sh
@@ -62,6 +62,6 @@ cp -fR ${WORKSPACE}/${ARGS[buildroot]}/TARBALL/*.tar.gz ${LOCAL_REPO_DIR}/${ARGS
 # below uses two utilities that will cause calls to createrepo (from this script) to wait to gain an
 # exclusive file lock before proceeding with a maximum wait time set at 10 minutes before they give
 # up and fail. the ha* commands are from the hatools rpm available via EPEL.
-#/usr/bin/hatimerun -t 10:00 \
-#/usr/bin/halockrun /var/lock/subsys/rpmrepo \
-/usr/bin/createrepo --update --workers 2 ${LOCAL_REPO_DIR}/${ARGS[repo]}/${ARGS[buildtype]}/${ARGS[arch]}/
+hatimerun -t 10:00 \
+halockrun /var/lock/subsys/rpmrepo \
+createrepo --update --workers 2 ${LOCAL_REPO_DIR}/${ARGS[repo]}/${ARGS[buildtype]}/${ARGS[arch]}/
--- deploy/packaging/docker/publish-vendor-rpm.sh
+++ deploy/packaging/docker/publish-vendor-rpm.sh
@@ -74,6 +74,6 @@ fi
 # below uses two utilities that will cause calls to createrepo (from this script) to wait to gain an
 # exclusive file lock before proceeding with a maximum wait time set at 10 minutes before they give
 # up and fail. the ha* commands are from the hatools rpm available via EPEL.
-#hatimerun -t 10:00 \
-#halockrun /var/lock/subsys/rpmrepo \
+hatimerun -t 10:00 \
+halockrun /var/lock/subsys/rpmrepo \
 createrepo --update --workers 2 ${LOCAL_REPO_DIR}/${ARGS[repo]}/${ARGS[buildtype]}/${ARGS[arch]}/
