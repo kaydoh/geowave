b50703a6116bf5d4b93f26eb88128652409a5f1b
Kent Miller <kmiller@radiantblue.com>
2016-12-08 09:00:24 -0800

    oops

--- .utility/run-tests.sh
+++ .utility/run-tests.sh
@@ -3,7 +3,7 @@ set -ev
 
 if [ "$IT_ONLY" == "true" ]; then
   echo -e "Skipping unit tests w/ verify...\n"
-  mvn -q verify -D -Dtest=SkipUnitTests -DfailIfNoTests=false -Dformatter.skip=true -Dfindbugs.skip=true -Daccumulo.version=$ACCUMULO_VERSION -Daccumulo.api=$ACCUMULO_API -Dhbase.version=$HBASE_VERSION -Dhadoop.version=$HADOOP_VERSION -Dgeotools.version=$GEOTOOLS_VERSION -Dgeoserver.version=$GEOSERVER_VERSION -P $PLATFORM_VERSION
+  mvn -q verify -Dtest=SkipUnitTests -DfailIfNoTests=false -Dformatter.skip=true -Dfindbugs.skip=true -Daccumulo.version=$ACCUMULO_VERSION -Daccumulo.api=$ACCUMULO_API -Dhbase.version=$HBASE_VERSION -Dhadoop.version=$HADOOP_VERSION -Dgeotools.version=$GEOTOOLS_VERSION -Dgeoserver.version=$GEOSERVER_VERSION -P $PLATFORM_VERSION
 else
   echo -e "Running unit tests only w/ verify...\n"
   mvn -q verify -DskipITs=true -Daccumulo.version=$ACCUMULO_VERSION -Daccumulo.api=$ACCUMULO_API -Dhbase.version=$HBASE_VERSION -Dhadoop.version=$HADOOP_VERSION -Dgeotools.version=$GEOTOOLS_VERSION -Dgeoserver.version=$GEOSERVER_VERSION -P $PLATFORM_VERSION
