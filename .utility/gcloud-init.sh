c52f82e0fb09447e3f9a73ed92a037a195a8a3f8
Kent Miller <kmiller@radiantblue.com>
2016-12-08 17:14:49 -0800

    update gcloud install"

--- .utility/gcloud-init.sh
+++ .utility/gcloud-init.sh
@@ -8,21 +8,21 @@ if [ "$STORE_TYPE" == "BIGTABLE" ]; then
 		gunzip google-cloud-sdk-136.0.0-linux-x86_64.tar.gz
 		tar xvf google-cloud-sdk-136.0.0-linux-x86_64.tar
 		rm google-cloud-sdk-136.0.0-linux-x86_64.tar
+
+		# install the beta component & emulator
+		cat <<EOF | ./google-cloud-sdk/bin/gcloud components install beta
+Y
+EOF
+
+		cat <<EOF | ./google-cloud-sdk/bin/gcloud beta emulators bigtable start &
+Y
+EOF
+
+		# get the current PID and kill it after a short wait
+		PID=$!
+		sleep 30
+		kill $PID
 	fi
-
-	# install the beta component & emulator
-	cat <<EOF | ./google-cloud-sdk/bin/gcloud components install beta
-Y
-EOF
-
-	cat <<EOF | ./google-cloud-sdk/bin/gcloud beta emulators bigtable start &
-Y
-EOF
-
-	# get the current PID and kill it after a short wait
-	PID=$!
-	sleep 30
-	kill $PID
 	
 	# run the following steps after this script completes:
 	# start the emulator
--- test/gcloud-init.sh
+++ test/gcloud-init.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 # Download the google cloud SDK
-wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-136.0.0-linux-x86_64.tar.gz
-gunzip google-cloud-sdk-136.0.0-linux-x86_64.tar.gz
-tar xvf google-cloud-sdk-136.0.0-linux-x86_64.tar
-rm google-cloud-sdk-136.0.0-linux-x86_64.tar
+if [ ! -d ./google-cloud-sdk ]; then
+	wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-136.0.0-linux-x86_64.tar.gz
+	gunzip google-cloud-sdk-136.0.0-linux-x86_64.tar.gz
+	tar xvf google-cloud-sdk-136.0.0-linux-x86_64.tar
+	rm google-cloud-sdk-136.0.0-linux-x86_64.tar
 
-# install the beta component
-cat <<EOF | ./google-cloud-sdk/bin/gcloud components install beta
+	# install the beta component
+	cat <<EOF | ./google-cloud-sdk/bin/gcloud components install beta
 Y
 EOF
 
-# the only way to install the bigtable emulator is to start it
-cat <<EOF | ./google-cloud-sdk/bin/gcloud beta emulators bigtable start &
+	# the only way to install the bigtable emulator is to start it
+	cat <<EOF | ./google-cloud-sdk/bin/gcloud beta emulators bigtable start &
 Y
 EOF
 
-# get the current PID and kill it after a short wait
-PID=$!
-sleep 30
-kill $PID
+	# get the current PID and kill it after a short wait
+	PID=$!
+	sleep 30
+	kill $PID
+fi
 
 # set the emulator host:port in the env
 export BIGTABLE_EMULATOR_HOST=localhost:8128
